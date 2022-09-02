08d3af046787fa0213b60d0a7f5f3931f2d82155
Andrew Ning <aning@byu.edu>
2022-09-02 12:06:07 -0600

    reversediff compatibility

--- src/implicit.jl
+++ src/implicit.jl
@@ -1,4 +1,6 @@
 using ForwardDiff
+using ReverseDiff
+using ChainRulesCore
 
 # ---------- Structs ---------
 
@@ -16,7 +18,7 @@ end
 
 # ------- Unpack/Pack ForwardDiff Dual ------
 """
-unpack ForwardDiff Dual
+unpack ForwardDiff Dual return value and derivative.
 """
 function unpack_dual(x) 
     xv = ForwardDiff.value.(x)
@@ -32,23 +34,25 @@ pack_dual(yv::AbstractVector, dy, T) = ForwardDiff.Dual{T}.(yv, ForwardDiff.Part
 
 
 
-# ---------- Functors ---------
+# ---------- Overloaded functions for implicit ---------
 
 """
     implicit(x)
 
-If not dual numbers, just solve normally.
+Functor, if not dual numbers, just solve normally.
 """
-(implicit::ImplicitFunction)(x) = implicit.solve(x)
+# (implicit::ImplicitFunction)(x) = implicit.solve(x)
+solve_implicit(implicit, x) = implicit.solve(x)
 
 
 """
     implicit(x)
 
-Overload for ForwardDiff inputs, providing exact derivatives using 
+Functor overload for ForwardDiff inputs, providing exact derivatives using 
 Jacobian vector product.
 """
-function (implicit::ImplicitFunction)(x::Vector{<:ForwardDiff.Dual{T}}) where {T}
+# function (implicit::ImplicitFunction)(x::Vector{<:ForwardDiff.Dual{T}}) where {T}
+function solve_implicit(implicit, x::Vector{<:ForwardDiff.Dual{T}}) where {T}
 
     # unpack dual numbers
     xv, dx = unpack_dual(x) 
@@ -72,9 +76,31 @@ function (implicit::ImplicitFunction)(x::Vector{<:ForwardDiff.Dual{T}}) where {T
 end
 
 
+# function ChainRulesCore.rrule(implicit::typeof(ImplicitFunction), x)
+function ChainRulesCore.rrule(::typeof(solve_implicit), implicit, x)
+    println("rule defined")
 
+    # evalute solvers
+    y = implicit(x)
 
-# ----------------------------------------
+    # compute residual partial derivative matrices
+    ry(ytilde) = implicit.residual(xv, ytilde)
+    rx(xtilde) = implicit.residual(xtilde, yv)
+
+    A = ForwardDiff.jacobian(ry, yv)  # computing partials with ForwardDiff (could use other methods)
+    B = ForwardDiff.jacobian(rx, xv)
+
+    function implicit_pullback(dy)
+        println("calling pullback")
+        u = transpose(A) \ dy
+        dx = -transpose(B) * u
+        return NoTangent(), NoTangent(), dx
+    end
+
+    return y, implicit_pullback
+end
+
+ReverseDiff.@grad_from_chainrules solve_implicit(implicit, x::TrackedArray)
 
 
 
@@ -109,6 +135,7 @@ end
 function lin_overload(x)
     y1 = 2.0*x
     lin_imp = ImplicitFunction(lin_solver, lin_residual)
+    
     y2 = lin_imp(y1)
     z = 2.0*y2
     return z
@@ -127,5 +154,11 @@ println("compute J1")
 J1 = ForwardDiff.jacobian(lin_overload, x)
 println("compute J2")
 J2 = ForwardDiff.jacobian(lin_ad_through, x)
+println("compute J3")
+J3 = ReverseDiff.jacobian(lin_overload, x)
+println("compute J4")
+J4 = ReverseDiff.jacobian(lin_ad_through, x)
 
 println(maximum(abs.(J1 - J2)))
+println(maximum(abs.(J3 - J2)))
+println(maximum(abs.(J4 - J2)))
